4d59030ded71b9cb70275a0cfe82941e338485f8
katia_biryla <kbiril@gmail.com>
2023-12-06 16:02:24 +0100

    Display a list step /html en ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import {AppComponent} from "./app.component"; // <-- NgModel lives here
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
