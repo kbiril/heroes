f1bdc3da4dcf197a18541e5f58e9fa60a0a7c42b
katia_biryla <kbiril@gmail.com>
2023-12-06 14:50:55 +0100

    Display a list step /html en ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -25,7 +25,6 @@ import {HEROES} from '../mock-heroes';
 export class HeroesComponent {
   heroes = HEROES;
   selectedHero?: Hero;
-
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
